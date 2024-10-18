b23cf19a7e0a195c994ec97027e3590c0b51b792
Zuhair Ansari <ansarizuhair145@gmail.com>
2024-10-18 09:32:43 +0530

    data ingestion and  model preparation components added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from ChestCancerDetectionAI import logger
 from ChestCancerDetectionAI.pipeline.stage_01_data_ingestion import data_ingestion_traning_pipeline
-
+from ChestCancerDetectionAI.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline  
 
 STAGE_NAME = "Data Ingestion Pipeline"
 
@@ -14,3 +14,15 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e   
+
+
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
