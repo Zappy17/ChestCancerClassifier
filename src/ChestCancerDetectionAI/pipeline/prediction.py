2f1bf191517620884c8e0a652735cc98f77e44cc
Zuhair Ansari <ansarizuhair145@gmail.com>
2024-11-08 03:17:21 +0530

    fixing some issues

--- src/ChestCancerDetectionAI/pipeline/prediction.py
+++ src/ChestCancerDetectionAI/pipeline/prediction.py
@@ -1,35 +1,33 @@
-import tensorflow
 import numpy as np
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
+import tensorflow as tf
 import os
 
-
-
 class PredictionPipeline:
-    def __init__(self,filename):
-        self.filename =filename
+    def __init__(self, filename):
+        self.filename = filename
 
-
-    
     def predict(self):
-        ## load model
-        
-        # model = load_model(os.path.join("artifacts","training", "model.h5"))
-        model = load_model(os.path.join("model", "model.h5"))
+        # Load model using TensorFlow
+        model = tf.keras.models.load_model(os.path.join("model", "model.h5"))
+
+        # Process the image using OpenCV (no need for keras.preprocessing)
+        import cv2
 
         imagename = self.filename
-        test_image = image.load_img(imagename, target_size = (224,224))
-        test_image = image.img_to_array(test_image)
-        test_image = np.expand_dims(test_image, axis = 0)
+        test_image = cv2.imread(imagename)
+        test_image = cv2.resize(test_image, (224, 224))  # Resize the image to match model's expected input
+        test_image = np.array(test_image)
+        test_image = np.expand_dims(test_image, axis=0)  # Add batch dimension
+
+        # Normalize the image if your model expects it (mean subtraction or scaling)
+        test_image = test_image / 255.0
+
+        # Predict the class
         result = np.argmax(model.predict(test_image), axis=1)
-        print(result)
 
         if result[0] == 1:
             prediction = 'Normal'
-            return [{ "image" : prediction}]
+            return [{"image": prediction}]
         else:
             prediction = 'Adenocarcinoma Cancer'
-            return [{ "image" : prediction}]
-        
-
+            return [{"image": prediction}]
